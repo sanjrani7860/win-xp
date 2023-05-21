474ca1b96e3f29700178ff7f36c778bb6ffd4184
sanjrani7860 <134188493+sanjrani7860@users.noreply.github.com>
2023-05-21 11:18:18 +0500

    Update Run2.sh

--- Run2.sh
+++ Run2.sh
@@ -7,7 +7,7 @@ wget -O XP.iso 'https://www.dropbox.com/s/eyz5apkkwqwtrvx/Windows%20XP%20Profess
 
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 sudo qemu-system-x86_64 \
-  -m 0.5G \
+  -m 0.25G \
   -cpu EPYC \
   -boot order=d \
   -drive file=XP.img \
