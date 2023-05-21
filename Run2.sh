66d2103c7f8462d0b8cbe9fdbfd3fa7b846c4012
sanjrani7860 <134188493+sanjrani7860@users.noreply.github.com>
2023-05-21 10:56:27 +0500

    Update Run2.sh

--- Run2.sh
+++ Run2.sh
@@ -1,13 +1,13 @@
 sudo apt-get update -y
 sudo apt install qemu-utils -y
 sudo apt-get install -y qemu-system-x86-64
-qemu-img create -f raw XP.img 64G
+qemu-img create -f raw XP.img 10G
 wget -O MyPal.iso 'https://drive.google.com/uc?&id=1S9TlVioj_AWcHbfaYoPd2B9GvACiIHvK&export=download'
 wget -O XP.iso 'https://www.dropbox.com/s/eyz5apkkwqwtrvx/Windows%20XP%20Professional%20%28x86%29.iso?dl=1'
 
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 sudo qemu-system-x86_64 \
-  -m 8G \
+  -m 1G \
   -cpu EPYC \
   -boot order=d \
   -drive file=XP.img \
@@ -17,7 +17,7 @@ sudo qemu-system-x86_64 \
   -device usb-tablet \
   -vnc :0 \
   -cpu coreduo \
-  -smp sockets=1,cores=4,threads=1 \
+  -smp sockets=1,cores=1,threads=1 \
   -vga std \
   -device rtl8139,netdev=n0 -netdev user,id=n0 \
   -accel tcg,thread=multi \
